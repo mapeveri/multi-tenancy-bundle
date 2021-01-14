9b2b8f0e014c43b912aa184a2ed370c7595598d8
Martin Peveri <martinpeveri@gmail.com>
2021-01-14 22:30:39 +0100

    Add test for doctrine/migration

---
+++ tests/Doctrine/Migration/DirectoryMigrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MultiTenancyBundle\Tests\Doctrine\Migration;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+use MultiTenancyBundle\Doctrine\Migration\DirectoryMigration;
+
+class DirectoryMigrationTest extends TestCase
+{
+    public function testGetConfiguration()
+    {
+        $fileMigrations = Yaml::parse("doctrine_migrations:
+            migrations_paths:
+                'DoctrineMigrations': 'migrations/Main'
+                'DoctrineMigrationsTenant': 'migrations/Tenant'
+        ");
+
+        $config = (array)DirectoryMigration::getConfiguration("tenant", $fileMigrations);
+        $value = reset($config);
+        $this->assertEquals("migrations/Tenant", $value['migrations_paths']['DoctrineMigrationsTenant']);
+
+        $config = (array)DirectoryMigration::getConfiguration("main", $fileMigrations);
+        $value = reset($config);
+        $this->assertEquals("migrations/Main", $value['migrations_paths']['DoctrineMigrations']);
+
+        $config = (array)DirectoryMigration::getConfiguration("error", $fileMigrations);
+        $this->assertEquals("migrations/Main", $value['migrations_paths']['DoctrineMigrations']);
+    }
+}
